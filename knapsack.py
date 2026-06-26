124b1a8c1adb04065e9ea8065f210d2af7db1440
TachikawaKaito <e245304@ie.u-ryukyu.ac.jp>
2026-06-26 16:44:18 +0900

    capacity スペル修正

--- knapsack.py
+++ knapsack.py
@@ -1,4 +1,4 @@
-capasity = 45
+capacity = 45
 size = [4, 8, 3, 5, 9, 2, 3, 1, 5, 2, 4, 2, 7, 10, 3, 13, 11, 8]
 price = [6, 12, 4, 3, 7, 1, 3, 2, 7, 3, 4, 2, 10, 13, 5, 16, 14, 9]
 
@@ -17,7 +17,7 @@ for i in range(2 ** n):
             total_size += size[j]
             total_price += price[j]
             bag.append(j + 1)
-    if ((total_price >= max_total_price) & (total_size <= 45)):
+    if ((total_price >= max_total_price) & (total_size <= capacity)):
         max_total_size = total_size
         max_total_price = total_price
         max_bag = bag
